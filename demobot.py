56b3e1316fc2c15c88ceead748c81772a3c36ac4
Thanh Van Ha Dang <tdan2481@shared.sydney.edu.au>
2019-01-10 16:04:59 +1100

    more changes

--- demobot.py
+++ demobot.py
@@ -1,15 +1,21 @@
 
 # Import flask
-from flask import Flask
+from flask import Flask, request
 
 # Create your app (web server)
 app = Flask(__name__)
 
-
+@app.route('/')
+def greet_person():
+    # Get the value of the 'text' query parameter
+    # request.values is a dictionary (cool!)
+    name = request.values.get('text')
+    # This bot says hi to every name it gets sent!
+    return f'hi {name}!'
 # When people visit the home page '/' use the hello_world function
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/1', methods=['GET', 'POST'])
 def text0():
-    return '<h2>AAAAAAAAAAAA!</h2>'
+    return 'testing, hi'
 
 @app.route('/ncss', methods=['GET', 'POST'])
 def text1():
@@ -17,5 +23,6 @@ def text1():
 
 if __name__ == '__main__':
     # Start the web server!
-    app.run()
+    app.run(debug=True)
+
 
